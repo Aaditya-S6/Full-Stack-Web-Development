a908ae960fdcf21ed99e2d52b60a74ec5dbabb6d
Aaditya Saraswat <saraswataaditya60@gmail.com>
2023-06-14 13:24:24 +0530

    First commit

---
+++ JavaScript/Intro/app.js
@@ -0,0 +1,23 @@
+let a="hello"
+let b=10
+console.log(a)
+console.log(b)
+
+
+//Naming Conventions
+// 1.Normal
+let samarth=100;
+let samvohra=1000;
+
+// 2.Camelcase
+let aadityaSaraswat=1000;
+let myNameIsAaditya=100;
+
+// 3.snakecase
+let aadi_bhai=1;
+let my_name_is_Aaditya=true;
+
+// 4.kabab case
+// let first-name='Aaditya' -->It is InValid
+
+
