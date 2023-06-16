dc2a2fc787e4153c34849ae8b32af59554be55b1
Aaditya Saraswat <saraswataaditya60@gmail.com>
2023-06-16 12:50:24 +0530

    Update app.js

--- JavaScript/Intro/app.js
+++ JavaScript/Intro/app.js
@@ -1,64 +1,108 @@
-let a="hello"
-let b=10
-console.log(a)
-console.log(b)
+// let a="hello"
+// let b=10
+// console.log(a)
+// console.log(b)
 
 
-//Naming Conventions
-// 1.Normal
-let samarth=100;
-let samvohra=1000;
+// //Naming Conventions
+// // 1.Normal
+// let samarth=100;
+// let samvohra=1000;
 
-// 2.Camelcase
-let aadityaSaraswat=1000;
-let myNameIsAaditya=100;
+// // 2.Camelcase
+// let aadityaSaraswat=1000;
+// let myNameIsAaditya=100;
 
-// 3.snakecase
-let aadi_bhai=1;
-let my_name_is_Aaditya=true;
+// // 3.snakecase
+// let aadi_bhai=1;
+// let my_name_is_Aaditya=true;
 
-// 4.kabab case
-// let first-name='Aaditya' -->It is InValid
+// // 4.kabab case
+// // let first-name='Aaditya' -->It is InValid
 
-let firstName='Aaditya';
-let anotherName=firstName;
-console.log(firstName)
-console.log(anotherName)
+// let firstName='Aaditya';
+// let anotherName=firstName;
+// console.log(firstName)
+// console.log(anotherName)
 
-let A='Aaditya';
-let B='Saraswat';
-let C=A+" "+B;
-console.log(C);
+// let A='Aaditya';
+// let B='Saraswat';
+// let C=A+" "+B;
+// console.log(C);
 
-//There is nothing like character
-// in JS. Even a single length word
-// is a String.
+// //There is nothing like character
+// // in JS. Even a single length word
+// // is a String.
 
-// typeof(variableName) -> Gives type of variable
+// // typeof(variableName) -> Gives type of variable
 
 
-let num1=10;
-let num2=10.10;
-let num3=-199;
-console.log(num1)
-console.log(num2)
-console.log(num3)
-console.log(typeof(num1))
-console.log(typeof(num2))
-console.log(typeof(num3))
+// let num1=10;
+// let num2=10.10;
+// let num3=-199;
+// console.log(num1)
+// console.log(num2)
+// console.log(num3)
+// console.log(typeof(num1))
+// console.log(typeof(num2))
+// console.log(typeof(num3))
 
-//Mathematical Operations
-let num4=num1*num2
-console.log(num4)
+// //Mathematical Operations
+// let num4=num1*num2
+// console.log(num4)
 
-// Rule (PEMDAS) > Operator Priority
-// P > parenthesis
-// E > Exponential
-// M > Mutiply
-// D > Divide
-// A > Addition 
-// S > Subtraction
-let ans=(1*2 - (3-4) + 3**2 /2)
-console.log(ans)
+// // Rule (PEMDAS) > Operator Priority
+// // P > parenthesis
+// // E > Exponential
+// // M > Mutiply
+// // D > Divide
+// // A > Addition 
+// // S > Subtraction
+// let ans=(1*2 - (3-4) + 3**2 /2)
+// console.log(ans)
+
+
+//Parameterised Functions
+// function sum(c){
+//     let a=100;
+//     let b=200;
+//     console.log(a+b+c);
+// }
+//sum(300)
+
+// ---return a value ---
+// function sum(){
+//     let a=100;
+//     let b=200;
+//     return (a+b);
+// }
+// console.log(sum());
+
+//--------multiple arguments ---------
+// function sum(a,b){
+//     return a+B; 
+// }
+// let ans1=sum(10,20);
+// console.log(ans1);
+
+//--default parameterised functions------
+// function sum(a,b,c=10){
+//     return a+b+c;
+// }
+// ans=sum(10,10);
+// console.log(ans)
+
+// // -----Over Riding parameters---------
+// ans=sum(10,10,1);
+// console.log(ans)
+
+//--------------
+// function sum(a,b){
+//     return a+b
+// }
+// console.log(sum());
+// When no parameter is given, then it assigns by
+//itself a value i.e,Undefined.Hence, the output
+//of above is NaN -> Not A Number.
 
 
